87b1d8155019a7db65fc71fa3f8b26078cdc4fbb
Alex <andres.a.campos@navy.mil>
2022-05-31 00:16:49 -0700

    fixing file paths, buttons now play sounds correctly

--- drumKit/drumKit.js
+++ drumKit/drumKit.js
@@ -1,19 +1,22 @@
-let drums = document.querySelectorAll(".button")
+let drums = document.querySelectorAll(".drum")
+
+console.log("drums", drums );
 
 let soundFiles = {
-    "w" : "/drumKit/sounds/crash.mp3",
-    "a" : "/drumKit/sounds/kick-bass.mp3tom-1.mp3",
-    "s" : "/drumKit/sounds/snare.mp3tom-1.mp3",
-    "d" : "/drumKit/sounds/tom-1.mp3tom-1.mp3",
-    "j" : "/drumKit/sounds/tom-2.mp3",
-    "k" : "/drumKit/sounds/tom-3.mp3",
-    "l" : "/drumKit/sounds/tom-4.mp3",
+    "w" : "sounds/crash.mp3",
+    "a" : "sounds/kick-bass.mp3",
+    "s" : "sounds/snare.mp3",
+    "d" : "sounds/tom-1.mp3",
+    "j" : "sounds/tom-2.mp3",
+    "k" : "sounds/tom-3.mp3",
+    "l" : "sounds/tom-4.mp3",
 }
-for (let i = 0; i < drums.length; i++) {
-    drums[i].addEventListener("click", () => {
-        this.style.color = "white"
 
-        let audio = new Audio(soundFiles[this.innerHTML])
+for (let i = 0; i < drums.length; i++) {
+    let drum = drums[i]
+    drum.addEventListener("click", () => {
+        drum.style.color = "white"
+        let audio = new Audio(soundFiles[drum.innerHTML])
         audio.play()
     })
 }
